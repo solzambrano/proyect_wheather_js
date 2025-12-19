708ce245222ef7ecfec6148ae13eb7607129385d
Sol Zambrano <solzambrano.8@gmail.com>
2025-12-19 18:18:40 -0300

    pantalla ok y error completadas

--- weather-api.js
+++ weather-api.js
@@ -1,14 +1,29 @@
 /**llamado de la api**/
-console.log('me estoy ejecutando bien ');
-let INFORMATION= {};
+let information= {};
+let bad_response=false;
+let block_error=document.getElementById('block-error');
+let block_information= document.getElementById("block-information")
+console.log(block_error);
+console.log(block_information);
+
+
 const callApi =async () => {
         const response=await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')
+        console.log(response);
+        
+        const{status,statusText}=response
         const data = await response.json()
-        return data
+        return {data,status,statusText}
 }
 
 const RenderPage = async() => {
-   INFORMATION = await callApi();
-  await  console.log('aqui traje la informacion', INFORMATION)
+   information = await callApi();
+    const {data,status,statusText} =  information
+    if(status== 200){
+    block_information.style.display='block'
+
+    }
+    else block_error.style.display="block"
+   
 }
 RenderPage()
