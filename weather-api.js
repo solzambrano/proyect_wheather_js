e451c34ff204d722ca7a8fa4370c498770ba5c9e
Sol Zambrano <solzambrano.8@gmail.com>
2025-12-22 12:37:03 -0300

    borrar logs innecesarios

--- weather-api.js
+++ weather-api.js
@@ -3,14 +3,9 @@ let information= {};
 let bad_response=false;
 let block_error=document.getElementById('block-error');
 let block_information= document.getElementById("block-information")
-console.log(block_error);
-console.log(block_information);
-
 
 const callApi =async () => {
-        const response=await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')
-        console.log(response);
-        
+        const response=await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')  
         const{status,statusText}=response
         const data = await response.json()
         return {data,status,statusText}
@@ -20,10 +15,10 @@ const RenderPage = async() => {
    information = await callApi();
     const {data,status,statusText} =  information
     if(status== 200){
-    block_information.style.display='block'
-
+    block_information.style.display='block';
     }
-    else block_error.style.display="block"
-   
+    else block_error.style.display="block";
+    console.log(data);
+    
 }
 RenderPage()
