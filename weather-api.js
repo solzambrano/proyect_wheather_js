1e5dc531dc2d5f9884e946a4020ba4bf2df757d7
Sol Zambrano <solzambrano.8@gmail.com>
2025-12-26 23:43:56 -0300

    entrada de nombre

--- weather-api.js
+++ weather-api.js
@@ -2,33 +2,39 @@
 let information= {};
 let bad_response=false;
 let block_error=document.getElementById('block-error');
-let block_information= document.getElementById("block-information")
+let block_information= document.getElementById("block-information");
+let search_input=document.getElementById("search-input")
+console.log('el serch',search_input);
+
 
 const callApi =async () => {
-        const response=await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')  
-        const{status,statusText}=response
-        const data = await response.json()
-        return {data,status,statusText}
+  const response=await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')  
+  const{status,statusText}=response
+  const data = await response.json()
+  return {data,status,statusText}
 }
 
 const RenderPage = async() => {
-   information = await callApi();
-    const {data,status,statusText} =  information
-    if(status== 200){
+  information = await callApi();
+  const {data,status,statusText} =  information
+  if(status== 200){
     block_information.style.display='block';
-    }
-    else block_error.style.display="block";
-    console.log(data);
-    
+  }
+  else block_error.style.display="block";
+  console.log(data);
+  
 }
 RenderPage()
 /*api para determinar latitud y longit segun texto*/
-const GetLocation = async () =>{
-    const response = await fetch('https://geocoding-api.open-meteo.com/v1/search?name=Argentina');
-    const{status,statusText}=response;
-    const data =await response.json()
-    console.log('respuesta ubicacion',data);
+const GetLocation = async (name) =>{
+  const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${name}`);
+  const{status,statusText}=response;
+  const data =await response.json()
+  console.log('respuesta ubicacion',data);
+  
+} 
 
-  } 
-
-  GetLocation()
+GetLocation()
+const GetName= (e) =>{ console.log('aqui esta el input',GetLocation(e.target.value));
+}
+search_input.addEventListener('change',GetName)
