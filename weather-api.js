5a5b313215a42fb4be093441edce3499f33b8c41
Sol Zambrano <solzambrano.8@gmail.com>
2025-12-29 22:49:30 -0300

    ajuste en try catch

--- weather-api.js
+++ weather-api.js
@@ -1,5 +1,4 @@
 /**declaración de variables**/
-let information= {};
 let bad_response=false;
 let block_error=document.getElementById('block-error');
 let block_information= document.getElementById("block-information");
@@ -11,33 +10,47 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const callApi =async () => {
-  const response=await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')  
-  const{status,statusText}=response
-  const data = await response.json()
-  return {data,status,statusText}
+  try{
+    const response=await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')  
+    if (response){
+      const{status,statusText}=response
+      const data = await response.json()
+      return {data,status,statusText}
+    }
+  }
+  catch(error){
+    console.log(error)
+  }
 }
 
 const RenderPage = async() => {
-  information = await callApi();
-  const {data,status,statusText} =  information
-  if(status== 200){
-    block_information.style.display='block';
+  try{
+   const information = await callApi();
+   console.log(information);
+   
+     const {data,status,statusText} =  information
+     if(status== 200 && data){
+       block_information.style.display='block';
+      }
+      else block_error.style.display="block";
+      console.log(data);
+    }
+   catch(error){
+    console.log(error)
   }
-  else block_error.style.display="block";
-  console.log(data);
   
 }
-RenderPage()
-/*api para determinar latitud y longit segun texto*/
-const GetLocation = async (name) =>{
-  const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${name}`);
-  const{status,statusText}=response;
-  const data =await response.json()
-  console.log('respuesta ubicacion',data);
+ RenderPage()
+// /*api para determinar latitud y longit segun texto*/
+// const GetLocation = async (name) =>{
+//   const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${name}`);
+//   const{status,statusText}=response;
+//   const data =await response.json()
+//   console.log('respuesta ubicacion',data);
   
-} 
+// } 
 
-GetLocation()
-const HandleGetName= () =>{ console.log('aqui esta el input',GetLocation(search_input.value));
-}
-search_button.addEventListener('click',HandleGetName)
+// GetLocation()
+// const HandleGetName= () =>{ console.log('aqui esta el input',GetLocation(search_input.value));
+// }
+// search_button.addEventListener('click',HandleGetName)
