65fa3f9c6cf713ee6610224fb588ba86a850ef23
Sol Zambrano <solzambrano.8@gmail.com>
2026-01-20 12:19:36 -0300

    preparando entorno para mostrar fecha

--- weather-api.js
+++ weather-api.js
@@ -1,18 +1,32 @@
 /**declaración de variables**/
 let bad_response=false;
+let date_formatted=new Date().toLocaleDateString('en',{
+weekday:'long',
+year:'numeric',
+month:'long',
+day:'numeric'
+})
+
 let block_error=document.getElementById('block-error');
 let block_information= document.getElementById("block-information");
 let search_input=document.getElementById("search-input");
 let search_button=document.getElementById("search-button");
-let name_location = document.getElementById("name-location")
+let retry=document.getElementById('retry');
+/**section data weather today */
+let name_location = document.getElementById("name-location");
+let temperature= document.getElementById('today-temp')
+let today= document.getElementById('today')
+let cloudy= ["../images/icon-drizzle.webp"]
+console.log(cloudy);
 
 document.addEventListener('DOMContentLoaded', () => {
   search_input.value = '';
+  RenderPage()
 });
 
 const callApi =async () => {
   try{
-    const response=await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')  
+    const response=await fetch('http://goweather.xyz/weather/Argentina')   
     if (response){
       const{status,statusText}=response
       const data = await response.json()
@@ -26,32 +40,35 @@ const callApi =async () => {
 
 const RenderPage = async() => {
   try{
-   const information = await callApi();
-   console.log(information);
-   
+   const information = await callApi();  
      const {data,status,statusText} =  information
      if(status== 200 && data){
        block_information.style.display='block';
       }
       else block_error.style.display="block";
-      console.log(data);
     }
    catch(error){
     console.log(error)
   }
   
 }
- RenderPage()
-// /*api para determinar latitud y longit segun texto*/
-// const GetLocation = async (name) =>{
-//   const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${name}`);
-//   const{status,statusText}=response;
-//   const data =await response.json()
-//   console.log('respuesta ubicacion',data);
-  
-// } 
+ 
 
-// GetLocation()
-// const HandleGetName= () =>{ console.log('aqui esta el input',GetLocation(search_input.value));
-// }
-// search_button.addEventListener('click',HandleGetName)
+const GetLocation = async (name) =>{
+  const response = await fetch(`http://goweather.xyz/weather/${name}`);
+  const{status,statusText}=response;
+  SetData(await response.json())
+  
+} 
+const HandleGetName= () =>{ console.log('aqui esta el input',GetLocation(search_input.value));
+}
+search_button.addEventListener('click',HandleGetName);
+retry.addEventListener('click',callApi)
+
+const SetData = (data) => {
+  console.log('respuesta ubicacion',data);
+  name_location.innerText=data.
+  temperature.innerText=data.temperature;
+  today.innerText=date_formatted
+
+}
