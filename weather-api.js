3233ec45d85641b8c18b9f2ee8342c3b6edc1168
Sol Zambrano <solzambrano.8@gmail.com>
2026-01-30 20:30:13 -0300

    primer intento de forecast section

--- weather-api.js
+++ weather-api.js
@@ -1,6 +1,7 @@
 /**declaración de variables**/
 let bad_response=false;
-let date_formatted=new Date().toLocaleDateString('en',{
+const date= new Date()
+let date_formatted=date.toLocaleDateString('en',{
 weekday:'long',
 year:'numeric',
 month:'long',
@@ -18,6 +19,7 @@ const ICONS= {
   clear:"./assets/images/icon-clear.svg"
 }
 
+
 let location_name='';
 let block_error=document.getElementById('block-error');
 let block_information= document.getElementById("block-information");
@@ -30,6 +32,11 @@ let temperature = document.getElementById('today-temp');
 let today = document.getElementById('today');
 let weather_img = document.getElementById('weather-img');
 let wind_number = document.getElementById('wind-number');
+// let day_forecast = document.getElementsByClassName('forecast-card');
+let forecast_card = document.getElementsByClassName('forecast-card');
+// let forecast_card = document.getElementsByClassName('forecast-card');
+
+
 document.addEventListener('DOMContentLoaded', () => {
   search_input.value = '';
   RenderPage()
@@ -57,7 +64,6 @@ const RenderPage = async(name) => {
   try{
     location_name= name ||'Argentina'
     const { data, status } = await callApi(location_name);
-console.log(data,'dat');
       if(status == 200 && data){
         block_information.style.display='block';
         name_location.innerText='Argentina'       
@@ -73,6 +79,7 @@ console.log(data,'dat');
 
 const HandleGetName= () =>{ RenderPage(search_input.value);}
 search_button.addEventListener('click',HandleGetName);
+/**boton reinicio, solamente funciona si la api falla por si sola, no funciona si el nombre esta mal */
 retry.addEventListener('click',RenderPage)
 
 
@@ -84,14 +91,31 @@ const SetIconWeather = (description) => {
   if(description.toLowerCase().includes('snow'))return ICONS.snow;
   if(description.toLowerCase().includes('rain'))return ICONS.rain;
   if(description.toLowerCase().includes('clear'))return ICONS.clear;
+}
 
+const setForecast = (forecast,data_weather,offset) => {
+  date.setDate(date.getDate()+offset+1)
+    const weekday = date.toLocaleDateString('en', {
+    weekday: 'short'
+  });
+  forecast.innerText=weekday;
+  forecast.src=SetIconWeather('sunny');
+  forecast.innerText=data_weather.temperature
+
+  console.log(forecast,data_weather,weekday);
 
 }
 const SetData = (data) => {
     console.log(data); 
-  name_location.innerText=location_name
-  temperature.innerText=data.temperature;
-  today.innerText=date_formatted;
-  weather_img.src = SetIconWeather(data.description)
-  wind_number.innerText=data.wind
+    let day=0;
+    name_location.innerText=location_name
+    temperature.innerText=data.temperature;
+    today.innerText=date_formatted;
+    weather_img.src = SetIconWeather(data.description)
+    wind_number.innerText=data.wind
+    for(forecast of forecast_card){
+      setForecast(forecast.children[day],data.forecast[day],day)
+      day++
+
+    }
 }
